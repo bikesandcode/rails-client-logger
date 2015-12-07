d19d363164c1885866c52fbeecc39a01e29e5d5d
Warren Taylor <warren@warrentaylor.ca>
2015-12-07 11:12:32 -0800

    Pure JS, just ran the .coffee through a compiler.

---
+++ vendor/assets/javascripts/rails_client_logger.js
@@ -0,0 +1,36 @@
+// Compiled coffeescript code from rails_client_logger
+window.jsLoggerBasePath || (window.jsLoggerBasePath = '');
+
+window.jsLoggerUrl || (window.jsLoggerUrl = "/logger/rails_client_logger/log");
+
+window.jsLogger = {
+  invoke: function(level, message) {
+    return jQuery.ajax({
+      type: 'post',
+      beforeSend: function(xhr) {
+        return xhr.setRequestHeader('X-CSRF-Token', jQuery('meta[name="csrf-token"]').attr('content'));
+      },
+      data: {
+        level: level,
+        message: message
+      },
+      complete: function(request) {},
+      url: "" + window.jsLoggerBasePath + window.jsLoggerUrl
+    });
+  },
+  debug: function(message) {
+    return this.invoke('debug', message);
+  },
+  info: function(message) {
+    return this.invoke('info', message);
+  },
+  warn: function(message) {
+    return this.invoke('warn', message);
+  },
+  error: function(message) {
+    return this.invoke('error', message);
+  },
+  fatal: function(message) {
+    return this.invoke('fatal', message);
+  }
+};
